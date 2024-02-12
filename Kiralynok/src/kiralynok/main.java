b16218993943b9685f733096821d91917dea65e8
leilalino <aleila0921@gmail.com>
2024-02-12 14:36:18 +0100

    UresSorok(), UresOszlopok(), UresSorokSzama() es UresOszlopokSzama()

--- Kiralynok/src/kiralynok/main.java
+++ Kiralynok/src/kiralynok/main.java
@@ -7,10 +7,17 @@ public class main {
     public static void main(String[] args) {
         
         Tabla t = new Tabla('*');
+        //4. feladat
         System.out.println(t.Megjelenit());
-        t.Elhelyez(5);
+        //6. feladat
+        t.Elhelyez(8);
         System.out.println(t.Megjelenit());
-        
+        //7. feladat
+        System.out.println(t.UresOszlop(7));
+        System.out.println(t.UresSor(1));
+        //8. feladat
+        System.out.println(t.UresOszlopokSzama());
+        System.out.println(t.UresSorokSzama());
         
     }
 }
--- Kiralynok/src/model/Tabla.java
+++ Kiralynok/src/model/Tabla.java
@@ -26,17 +26,53 @@ public class Tabla {
         return tablazat;
     }
 
-    public void Elhelyez(int db) {
-    int i = 0;
-    while (i < db) {
-        int x = (int) (Math.random() * 8);
-        int y = (int) (Math.random() * 8);
-        if (this.T[x][y] == this.UresCella) {
-            this.T[x][y] = 'K';
-            i++;
+    public void Elhelyez(int N) {
+        int i = 0;
+        while (i < N) {
+            int egyik = (int) (Math.random() * 8);
+            int masik = (int) (Math.random() * 8);
+            if (this.T[egyik][masik] == this.UresCella) {
+                this.T[egyik][masik] = 'K';
+                i++;
+            }
         }
     }
-}
+
+    public boolean UresOszlop(int m) {
+        int i = 0;
+        while (i <= 7 && this.T[i][m] == this.UresCella) {
+            i++;
+        }
+        return !(i <= 7);
+    }
+
+    public boolean UresSor(int m) {
+        int i = 0;
+        while (i <= 7 && this.T[m][i] == this.UresCella) {
+            i++;
+        }
+        return !(i <= 7);
+    }
+
+    public int UresOszlopokSzama() {
+        int db = 0;
+        for (int i = 0; i <= 7; i++) {
+            if (UresOszlop(i)) {
+                db++;
+            }
+        }
+        return db;
+    }
+
+    public int UresSorokSzama() {
+        int db = 0;
+        for (int i = 0; i <= 7; i++) {
+            if (UresSor(i)) {
+                db++;
+            }
+        }
+        return db;
+    }
 
     public char[][] getT() {
         return T;
