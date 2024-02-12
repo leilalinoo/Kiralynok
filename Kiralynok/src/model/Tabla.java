faae623e1b1ad8425a23287e1426166343ebe5a5
leilalinoo <115218496+leilalinoo@users.noreply.github.com>
2024-02-12 13:42:47 +0100

    modell konstruktor

--- Kiralynok/src/model/Tabla.java
+++ Kiralynok/src/model/Tabla.java
@@ -1,6 +1,29 @@
 package model;
 
 public class Tabla {
-    private String[][] T = new String[8][8];
+    private char[][] T = new char[8][8];
+    private char UresCella;
+    
+    public Tabla(char c) {
+        this.UresCella = c;
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                this.T[i][j] = this.UresCella;
+            }
+        }
+    }
+//    
+//     public void Megjelenit() {
+//        for (int i = 0; i < 8; i++) {
+//            for (int j = 0; j < 8; j++) {
+//                System.out.print(T[i][j]);
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+//    }
+//     
+//    
     
 }
